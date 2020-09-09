b89700c2019689189b05b000c6d58909b15b3098
LarsNoordman01 <noordmanlars@gmail.com>
2020-09-09 09:28:59 +0000

    les 9 september, 1 bal werkt

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+ var x = 100;
+ var y = 100;
+ var dx = 20;
+ var dy = 20;
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  
 }
 
 
@@ -18,9 +22,24 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+    
   // stel vulkleur in
   fill(100, 100, 255);
-
+background('blue');
   // teken een cirkel
-  ellipse(50,50,80,80);
+  
+  ellipse(x,y,80,80);
+ 
+x += dx;
+y += dy;
+
+if(x >= 1240 || x <= 40){
+    dx = -dx;
+}
+
+if(y >=680 || y <= 40){
+    dy = -dy;
+}
+  //bewegen
+ 
 }
